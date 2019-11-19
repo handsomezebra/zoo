3b0876d983434df9ded9b8a88aac6a2477ba08d7
Handsome Zebra <aaahchi@gmail.com>
2019-11-19 15:36:16 -0800

    Adding duckling

---
+++ duckling/README.md
@@ -0,0 +1,11 @@
+# duckling
+
+Duckling is a Haskell library that parses text into structured data.
+
+## Usage
+1. build
+`docker build -t zoo_duckling .` 
+2. run
+`docker run -d -p 10000:10000 zoo_duckling`
+3. test
+`pytest` 
---
+++ duckling/test/test_api.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+
+import json
+import requests
+import logging
+import csv
+
+url = "http://localhost:10000/parse"
+
+
+def get_result(text, lang, dims, latent=None, reftime=None, tz=None):
+    data = {
+        "text": text,
+        "lang": lang,
+        "dims": json.dumps(dims)
+    }
+
+    if reftime is not None:
+        data["reftime"] = reftime
+
+    if tz is not None:
+        data["tz"] = tz
+
+    if latent is not None:
+        data["latent"] = latent
+
+    response = None
+
+    try:
+        response = requests.post(url, data=data)
+        response.raise_for_status()
+    except requests.exceptions.RequestException as e:
+        logging.warning("Duckling service %s requests exception: %s", url, e)
+
+    if response is None:
+        logging.warning("Failed to call translation service")
+        return None
+    elif response.status_code != 200:
+        logging.warning("Invalid response code %d from translation service", response.status_code)
+        return None
+    else:
+        return response.json()
+
+
+def test_time_en():
+
+    reftime = "1559920354000"  # 6/7/2019 8:12:34 AM
+    time_zone = "America/Los_Angeles"
+
+    result = get_result("tomorrow at eight", "en", ["time"], reftime=reftime)
+
+    assert result is not None and result[0]["value"]["value"] == "2019-06-08T08:00:00.000-07:00"
